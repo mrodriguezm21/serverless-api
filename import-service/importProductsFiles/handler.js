90eed63883216a896848d5551c2314b4ad617786
Mario <mariojosexd@gmail.com>
2024-01-03 10:00:48 -0500

    task 5.1

---
+++ import-service/importProductsFiles/handler.js
@@ -0,0 +1,23 @@
+import { createResponse } from "../libs/response.js";
+import { PutObjectCommand, GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import { getSignedUrl }  from "@aws-sdk/s3-request-presigner";
+
+const client = new S3Client({});
+
+export const importProductsFile = async (event, context) => {
+  const fileName = event.queryStringParameters.name;
+  const command = new PutObjectCommand({
+    Bucket: process.env.BUCKET,
+    Key: `uploaded/${fileName}`,
+  });
+
+  try {
+    const url = await getSignedUrl(client, command, { expiresIn: 15 * 60 });
+    // console.log("command", command);
+    // const response = await client.send(command);
+    // console.log("response", response);
+    return createResponse(200, url);
+  } catch (err) {
+    console.error(err);
+  }
+};
